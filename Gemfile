50a3c1d04bf009638d94bc666222daa3736ba669
Paul Chobert <paul@chobert.fr>
2012-05-02 13:22:08 +0200

    program draft

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "http://rubygems.org"
+
+gem 'capybara-webkit'
+gem 'pry'
---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'bundler/setup'
+
+require 'capybara/dsl'
+require 'capybara-webkit'
+require 'pry'
+
+include Capybara::DSL
+Capybara.current_driver = :webkit
+Capybara.app_host = 'https://docs.google.com/'
+page.visit('/a/chobert.fr/spreadsheet/viewform?formkey=dHd6a0tsZlBoTG5Hd0k4TU4tVm5XVmc6MQ')
+
+votes = [3, 2, 3, 4, 2, 1, 1, 5, 2, 4, 3]
+votes.each_with_index do |vote, index|
+  page.choose "group_#{index}_#{vote}"
+end
+
+page.click_button 'Envoyer'
+
+binding.pry
