2455789dc6cd4e4325495548b3a4ea70ef55617e
Paul Chobert <paul@chobert.fr>
2012-05-02 14:57:32 +0200

    add rand option

--- app.rb
+++ app.rb
@@ -9,9 +9,16 @@ Capybara.current_driver = :webkit
 Capybara.app_host = 'https://docs.google.com/'
 page.visit('/a/chobert.fr/spreadsheet/viewform?formkey=dHd6a0tsZlBoTG5Hd0k4TU4tVm5XVmc6MQ')
 
-votes = [3, 2, 3, 4, 2, 1, 1, 5, 2, 4, 3]
-votes.each_with_index do |vote, index|
-  page.choose "group_#{index}_#{vote}"
+if ARGV[0] == 'RAND'
+  11.times do |i|
+    n_elements = page.all("[@name='entry.#{i}.group']").count
+    page.choose "group_#{i}_#{Random.rand(1..n_elements)}"
+  end
+else
+  votes = [3, 2, 3, 4, 2, 1, 1, 5, 2, 4, 3]
+  votes.each_with_index do |vote, index|
+    page.choose "group_#{index}_#{vote}"
+  end
 end
 
 begin
